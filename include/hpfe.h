3f7814747493f853809a8ed6a6cd8b0437eb3668
akos011221 <orbanakos2001@gmail.com>
2026-01-01 23:47:39 +0000

    feat: initial commit

---
+++ include/hpfe.h
@@ -0,0 +1,15 @@
+#ifndef HPFE_H
+#define HPFE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct
+{
+    const char *iface; // interface name
+    int verbose;       // 0..2
+    int duration_sec;  // 0 = run forever
+} hpfe_config_t;
+
+#endif
---
+++ include/log.h
@@ -0,0 +1,22 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdarg.h>
+
+typedef enum {
+    LOG_ERROR = 0,
+    LOG_WARN = 1,
+    LOG_INFO = 2,
+    LOG_DEBUG = 3
+} log_level_t;
+
+void log_set_level(log_level_t level);
+
+/*
+  printf-style logging.
+  Example:
+    log_msg(LOG_INFO, "Listening on %s", iface);
+*/
+void log_msg(log_level_t level, const char *fmt, ...);
+
+#endif
